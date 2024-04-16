337dd158e8ad4ebe118306bc2645492b09a16806
Lareine Han <lareine.han@gmail.com>
2024-04-16 11:48:19 -0700

    props updated with sort option

--- src/pages/Listing/Listing.jsx
+++ src/pages/Listing/Listing.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Listing.style.css';
 import Properties from '../../common/Properties/Properties';
+import Map from './components/Map/Map';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Listing = () => {
 	const searchParams = {
@@ -8,12 +10,18 @@ const Listing = () => {
 		minRent: '1500',
 		maxRent: '3000',
 		page: '1',
+		sort: 'default', // Sort has options on the API!
 		// Add other params here
 	};
 	return (
-		<div className='properties'>
-			<Properties props={searchParams} />
-		</div>
+		<Container className='properties'>
+			<Col lg={8} className='property-listing-markers'>
+				<Map />
+			</Col>
+			<Col lg={4} className='property-listing'>
+				<Properties props={searchParams} />
+			</Col>
+		</Container>
 	);
 };
 
