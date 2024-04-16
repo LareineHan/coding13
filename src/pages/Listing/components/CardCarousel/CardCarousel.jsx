8035b748f9c5862b5aceb5e83e65c13a45b1a9bb
sapiens12 <sapiens12@nate.com>
2024-04-17 00:33:14 +0200

    리스트 페이지 캐러셀 이미지 추가

---
+++ src/pages/Listing/components/CardCarousel/CardCarousel.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useGetImagesQuery } from "../../../../hooks/useGetPropertyImages";
+import Carousel from "react-bootstrap/Carousel";
+
+const CardCarousel = ({ id }) => {
+  const { data, isLoading, isError } = useGetImagesQuery(id);
+  console.log("property images:", data);
+
+  if (isLoading) {
+    return <div>Loading images...</div>;
+  }
+
+  if (isError) {
+    return <div>Error fetching images: {isError.message}</div>;
+  }
+
+  return (
+    <div>
+      <Carousel fade>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src={data?.data[0].link}
+            alt='First slide'
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src={data?.data[data.data.length - 1].link}
+            alt='First slide'
+          />
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
+
+export default CardCarousel;
