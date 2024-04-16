81536b42ef456fbb31241b880c730daad7c7337a
MJJUNG33 <jung2674@gmail.com>
2024-04-16 13:09:15 +0900

    Create fetchDetailProperty slicer

---
+++ src/PropertyDeatil/PropertyDetailPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./PropertyDetailPage.style.css";
+
+const PropertyDetailPage = () => {
+  return <div>PropertyDetailPage</div>;
+};
+
+export default PropertyDetailPage;
--- src/redux/reducers/getPropertiesSlice.js
+++ src/redux/reducers/getPropertiesSlice.js
@@ -1,49 +1,82 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import api from '../../utils/api';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import api from "../../utils/api";
 
 let initialState = {
-	properties: [],
-	status: 'idle',
-	error: null,
+  properties: [],
+  detailProperty: null,
+  isLoading: false,
+  status: "idle",
+  error: null,
 };
 
 export const fetchProperties = createAsyncThunk(
-	'properties/fetchProperties',
-	async (params) => {
-		// Accept params object
-		try {
-			const response = await api.get('/properties', {
-				params: {
-					...params, // Spread the params object here
-					sort: 'default', // Default sort
-				},
-			});
-			return response.data;
-		} catch (error) {
-			throw error;
-		}
-	}
+  "properties/fetchProperties",
+  async (params) => {
+    // Accept params object
+    try {
+      const response = await api.get("/properties", {
+        params: {
+          ...params, // Spread the params object here
+          sort: "default", // Default sort
+        },
+      });
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+);
+
+export const fetchDetailProperty = createAsyncThunk(
+  "properties/fetchDetailProperty",
+  async (id, thunkAPI) => {
+    try {
+      console.log("Fetching detail property with ID:", id);
+      const url = `/properties/${id}`;
+      const response = await api.get(url);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
 );
 
 const getPropertiesSlice = createSlice({
-	name: 'properties',
-	initialState,
+  name: "properties",
+  initialState,
+  reducers: {},
 
-	extraReducers: (builder) => {
-		builder
-			.addCase(fetchProperties.pending, (state) => {
-				state.status = 'loading';
-			})
-
-			.addCase(fetchProperties.fulfilled, (state, action) => {
-				state.status = 'succeeded';
-				state.properties = action.payload;
-			})
-			.addCase(fetchProperties.rejected, (state, action) => {
-				state.status = 'failed';
-				state.error = action.error.message;
-			});
-	},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProperties.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchProperties.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.status = "succeeded";
+        state.properties = action.payload;
+      })
+      .addCase(fetchProperties.rejected, (state, action) => {
+        state.isLoading = false;
+        state.status = "failed";
+        state.error = action.error.message;
+      })
+      .addCase(fetchDetailProperty.pending, (state) => {
+        state.isLoading = true;
+        state.status = "pending";
+      })
+      .addCase(fetchDetailProperty.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.status = "succeeded";
+        state.detailProperty = action.payload;
+        console.log("detail product", action.payload);
+      })
+      .addCase(fetchDetailProperty.rejected, (state, action) => {
+        state.isLoading = false;
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
 });
 
 export default getPropertiesSlice.reducer;
