071ebe114353767eaa84a3fc456a705ec5563141
geunseonkim <guensoen@gmail.com>
2024-04-16 02:34:13 -0400

    add various files and set-up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios';
+
 
 
 function App() {
--- src/MinjiTest.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MinjiTest = () => {
-  return <div>MinjiTest</div>;
-};
-
-export default MinjiTest;
