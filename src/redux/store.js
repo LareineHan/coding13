83b15e6ea63ee7b0188b2a592457016db2cebdbb
jiylee <jjenny4711@gmail.com>
2024-04-18 05:24:05 -0400

    push api and redux first

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import getPropertiesSlice from './reducers/getPropertiesSlice';
 import getMapMarkersSlice from './reducers/getMapMarkersSlice';
-
+import authReducer from './reducers/authReducer';
+import likeReducer from './reducers/likeReducer';
 const store = configureStore({
 	reducer: {
 		properties: getPropertiesSlice,
 		mapMarkers: getMapMarkersSlice,
+		user:authReducer,
+		like:likeReducer,
 	},
 });
 export default store;
---
+++ src/utils/apibk.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+const apibk = axios.create({
+  baseURL: 'http://localhost:5000/coding',
+  headers: {
+  
+   "Content-Type": "application/json",
+  
+	},
+})
+
+apibk.interceptors.request.use(function (config) {
+  // Do something before request is sent
+
+  return config;
+}, function (error) {
+  // Do something with request error
+
+  return Promise.reject(error);
+});
+
+// Add a response interceptor
+apibk.interceptors.response.use(function (response) {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+
+  return response;
+}, function (error) {
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  console.log('res err',error)
+  return Promise.reject(error);
+});
+
+export default apibk;
