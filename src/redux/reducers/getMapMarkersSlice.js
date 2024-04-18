79e513a6cb924c68702c3afcd2bef2b0589c0af0
Lareine Han <lareine.han@gmail.com>
2024-04-18 13:53:33 -0700

    get current location / default to new york / search ? searched result

--- src/redux/reducers/getMapMarkersSlice.js
+++ src/redux/reducers/getMapMarkersSlice.js
@@ -3,6 +3,7 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 let initialState = {
 	markers: [],
 	status: 'idle',
+	loading: true,
 	error: null,
 };
 
@@ -27,16 +28,19 @@ const getMapMarkersSlice = createSlice({
 		builder
 			.addCase(addMapMarkers.pending, (state) => {
 				state.status = 'loading';
+				state.loading = true;
 				console.log('addMapMarkers.pending', state.status);
 			})
 
 			.addCase(addMapMarkers.fulfilled, (state, action) => {
 				state.status = 'succeeded';
+				state.loading = false;
 				state.markers = action.payload;
 				console.log('addMapMarkers.fulfilled', state.markers);
 			})
 			.addCase(addMapMarkers.rejected, (state, action) => {
 				state.status = 'failed';
+				state.loading = false;
 				state.error = action.error.message;
 				console.log('addMapMarkers.rejected', state.error);
 			});
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import getPropertiesSlice from './reducers/getPropertiesSlice';
 import getMapMarkersSlice from './reducers/getMapMarkersSlice';
 import getSearchPlaceSlice from './reducers/getSearchPlaceSlice';
+import getUserLocationSlice from './reducers/getUserLocationSlice';
 
 const store = configureStore({
 	reducer: {
 		properties: getPropertiesSlice,
 		mapMarkers: getMapMarkersSlice,
 		searchPlace: getSearchPlaceSlice,
+		userLocation: getUserLocationSlice,
+		showUserLocation: getUserLocationSlice,
 	},
 });
 export default store;
