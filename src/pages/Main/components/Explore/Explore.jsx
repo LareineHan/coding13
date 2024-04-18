16129ed1b3fd871344dbcfc4ca4d849986e10e24
geunseonkim <guensoen@gmail.com>
2024-04-18 03:00:11 -0700

    check Explore

--- src/pages/Main/components/Explore/Explore.jsx
+++ src/pages/Main/components/Explore/Explore.jsx
@@ -10,9 +10,9 @@ const Explore = () => {
       <h2>Explore Rentals in Mississauga, ON</h2>
       <div className='explore-cards'>
         <ExploreCard/>
+        {/* <ExploreCard/>
         <ExploreCard/>
-        <ExploreCard/>
-        <ExploreCard/>
+        <ExploreCard/> */}
       </div>
       <Button variant="success" type="submit" className='explore-more-btn'>View More</Button>
     </div>
