123072ee928526d7ac79b37d618694a12f2d3776
MJJUNG33 <jung2674@gmail.com>
2024-04-18 14:47:35 +0900

    Import all components that crreate newly, add selectedTab usestate and useeffect for PropertyPricing component to work tab and content accordingly

--- src/pages/PropertyDetail/PropertyDetailPage.js
+++ src/pages/PropertyDetail/PropertyDetailPage.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./PropertyDetailPage.style.css";
 import { useGetPropertyDetailQuery } from "../../hooks/useGetPropertyDetail";
 import { useParams } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
@@ -7,6 +8,11 @@ import { useGetImagesQuery } from "../../hooks/useGetPropertyImages";
 import PropertyCarousel from "../../common/Carousel/PropertyCarousel";
 import { useGetReviewsQuery } from "../../hooks/useGetReviews";
 import PropertyRating from "../../common/PropertyRating/PropertyRating";
+import PropertyPricing from "./components/PropertyPricing";
+import Contact from "./components/Contact";
+import Amenities from "./components/Amenities";
+import FeesPolicies from "./components/FeesPolicies";
+import PropertyDetails from "./components/PropertyDetails";
 
 const PropertyDetailPage = () => {
   const { id } = useParams();
@@ -14,7 +20,17 @@ const PropertyDetailPage = () => {
   const { data: imageData } = useGetImagesQuery(id);
   const { data: reviewsData } = useGetReviewsQuery(id);
 
+  const [selectedTab, setSelectedTab] = useState(null);
+
+  useEffect(() => {
+    // Set the default selected tab when data or availabilities change
+    if (data && data.availabilities && data.availabilities.length > 0) {
+      setSelectedTab(data.availabilities[0].type); // Select the type of the first availability
+    }
+  }, [data]);
+
   console.log("PropertyDetailData", data);
+  console.log("PropertyDetailData", reviewsData);
 
   if (isLoading) {
     return (
@@ -45,16 +61,29 @@ const PropertyDetailPage = () => {
     name,
     rentRange,
     bedRange,
+    bathRange,
     address,
     description,
     contact,
     amenities,
-    transits,
+    availabilities,
+    recurringExpenses,
+    leaseTerms,
+    yearBuilt,
+    unitCount,
+    storyCount,
+    isFurnished,
+    // transits,
   } = data; // Access nested data
 
-  const images = imageData?.data || [];
-  const reviews = reviewsData?.data || [];
+  const images = imageData?.data;
+  const reviews = reviewsData?.data;
 
+  const handleTabSelect = (type) => {
+    setSelectedTab(type);
+  };
+
+  //Calculate average rating based on total reviews of property
   const calculateAverageRating = () => {
     if (reviews.length === 0) return 0;
 
@@ -64,7 +93,7 @@ const PropertyDetailPage = () => {
 
   const averageRating = calculateAverageRating();
 
-  const totalReviews = reviews.length;
+  const totalReviews = reviews.length; //total number of reviews
 
   return (
     <Container>
@@ -79,26 +108,50 @@ const PropertyDetailPage = () => {
         totalReviews={totalReviews}
       />
 
-      <p>Rent Range: {rentRange}</p>
-      <p>Bed Range: {bedRange}</p>
-      <p>Description: {description}</p>
-      {/* Display Property Reviews */}
+      <div className="rent-bed-bath-range-container">
+        <div className="rent-bed-bath-range-inner-container">
+          <p className="rent-range-title">Rent Range</p>
+          <p className="rent-range-value">{rentRange}</p>
+        </div>
+
+        <div className="rent-bed-bath-range-inner-container">
+          <p className="bed-range-title">Bed Range</p>
+          <p className="bed-range-value">{bedRange}</p>
+        </div>
+
+        <div className="rent-bed-bath-range-inner-container">
+          <p className="bath-range-title">Bath Range</p>
+          <p className="bath-range-value">{bathRange}</p>
+        </div>
+      </div>
+
+      <PropertyPricing
+        availabilities={availabilities}
+        selectedTab={selectedTab}
+        onTabSelect={handleTabSelect}
+      />
+
+      <div>
+        <h2>About {name}</h2>
+        <p>{description}</p>
+      </div>
+
+      <Contact contact={contact} />
+
+      <Amenities amenities={amenities} />
+
+      <FeesPolicies data={data} />
+
       <PropertyReviews id={id} />
-      <h2>Contact</h2>
-      <p>Phone: {contact.phone}</p>
-      <p>Company: {contact.name}</p>
-      <h2>Amenities</h2>
-      <ul>
-        {amenities.map((amenity) => (
-          <li key={amenity.id}>{amenity.type}</li>
-        ))}
-      </ul>
-      <h2>Transit Options</h2>
-      <ul>
-        {transits.map((transit) => (
-          <li key={transit.id}>{transit.type}</li>
-        ))}
-      </ul>
+
+      <PropertyDetails
+        recurringExpenses={recurringExpenses}
+        leaseTerms={leaseTerms}
+        yearBuilt={yearBuilt}
+        unitCount={unitCount}
+        storyCount={storyCount}
+        isFurnished={isFurnished}
+      />
     </Container>
   );
 };
