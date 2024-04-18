a45fcc7e41e630272bfef0bf251c04bddcc013d2
geunseonkim <guensoen@gmail.com>
2024-04-18 14:29:26 -0700

    delete footer from Main components

--- src/pages/Main/Mainpage.jsx
+++ src/pages/Main/Mainpage.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Banner from './components/Banner/Banner'
 import Explore from './components/Explore/Explore'
 import Information from './components/Information/Information'
-// import Footer from './components/Footer/Footer'
 import Adventure from './components/Adventure/Adventure'
 
 const Mainpage = () => {
@@ -13,7 +12,6 @@ const Mainpage = () => {
       <Explore/>
       <Adventure/>
       <Information/>
-      {/* <Footer/> */}
     </div>
   )
 }
