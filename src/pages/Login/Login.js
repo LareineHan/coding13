5054abde740310bf0749cf9f204495079b81984d
jiylee <jjenny4711@gmail.com>
2024-04-19 17:03:23 -0400

    add return

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -4,21 +4,23 @@ import { googleLogin } from '../../redux/Actions/authAction';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 const Login = () => {
-  const navigate=useNavigate()
-  const dispatch=useDispatch()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   useGoogleOneTapLogin({
-    onSuccess: credentialResponse => {
-     
-      dispatch(googleLogin({token:credentialResponse.credential}))
-     navigate('/')
+    onSuccess: async credentialResponse => {
+      try {
+       await dispatch(googleLogin({ token: credentialResponse.credential }));
+        return navigate('/');
+      } catch (error) {
+        console.error('Navigation error:', error);
+      }
     },
     onError: () => {
       console.log('Login Failed');
     },
   });
-
-
-  
 }
+
 
 export default Login;
