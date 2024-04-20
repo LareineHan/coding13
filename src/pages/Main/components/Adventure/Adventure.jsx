995b579323b7ee8d2b9a7c62d5379cf7026006f6
jiylee <jjenny4711@gmail.com>
2024-04-20 13:57:38 -0400

    edit sign in btn

--- src/pages/Main/components/Adventure/Adventure.jsx
+++ src/pages/Main/components/Adventure/Adventure.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import AdventureCarousel from '../AdventureCarousel/AdventureCarousel'
 // import AdventureCard from '../AdventureCard/AdventureCard'
 import "./Adventure.style.css"
+import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 
 
 const Adventure = () => {
+  const token=sessionStorage.getItem('token')
+  
   return (
     <div>
         <div className='adventure-container' style={{display: "flex", justifyContent: "center", alignContent: "center"}}>
@@ -13,7 +16,9 @@ const Adventure = () => {
                 <div style={{marginTop: "9rem", marginRight: "2rem", textAlign: "center"}}>
                     <h4>Get home recommendations</h4>
                     <p>Sign in for a more personalized experience.</p>
-                    <Button variant="outline-success" type="submit" className='adventure-more-btn'>Sign in</Button>
+                    <Link to={!token?'/login':'/properties'} style={{textDecoration: 'none'}}>
+                    <Button variant="outline-success" type="submit" className='adventure-more-btn'>{!token?'Sign in':'Go to Properties'}</Button>
+                    </Link>
                 </div>
             </div>
             <AdventureCarousel/>
