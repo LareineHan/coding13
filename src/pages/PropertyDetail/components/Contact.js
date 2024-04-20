8b3cc8b13453f08bb87915bf55324b9d1ee48733
MJJUNG33 <jung2674@gmail.com>
2024-04-20 19:48:09 +0900

    Add icon and change colour of button and style

--- src/pages/PropertyDetail/components/Contact.js
+++ src/pages/PropertyDetail/components/Contact.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Contact.style.css";
 import { Container, Row, Col, Button } from "react-bootstrap";
+import contactIcon from "../Icons/telephone-call.png";
+
 const Contact = ({ contact }) => {
   return (
     <Container className="contact-container">
+      <h2>
+        Contact{" "}
+        <img src={contactIcon} alt="contact icon" className="contactIcon" />
+      </h2>
       <Row className="contactName-logo">
-        <h2>Contact</h2>
         <Col xs={8}>
           <h3>{contact.name}</h3>
         </Col>
@@ -20,7 +25,7 @@ const Contact = ({ contact }) => {
           <p>{contact.phone}</p>
         </Col>
       </Row>{" "}
-      <Button className="message-btn">Message</Button>
+      <Button className="contact-message-btn">Message</Button>
     </Container>
   );
 };
