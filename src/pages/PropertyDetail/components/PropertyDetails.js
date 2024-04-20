1b0111e9dbab6d14aee9fa4a3bbca50de3038a24
MJJUNG33 <jung2674@gmail.com>
2024-04-20 22:22:02 +0900

    Change list layout to devide as 2 columns

--- src/pages/PropertyDetail/components/PropertyDetails.js
+++ src/pages/PropertyDetail/components/PropertyDetails.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./PropertyDetails.style.css";
+import { Container } from "react-bootstrap";
 
 const PropertyDetails = ({
   recurringExpenses,
@@ -8,6 +9,7 @@ const PropertyDetails = ({
   unitCount,
   storyCount,
   isFurnished,
+  propertyInfo,
 }) => {
   const getLeaseTermList = (terms) => {
     if (!terms) return [];
@@ -17,40 +19,51 @@ const PropertyDetails = ({
   const leaseTermList = getLeaseTermList(leaseTerms);
 
   return (
-    <div>
+    <Container>
       <h2>Details</h2>
-      <p className="utility-title">Utility Included</p>
-      <ul>
-        {recurringExpenses.map((recurringExpense, index) => {
-          // Split the type string by comma, ampersand, or 'and'
-          const formattedWords = recurringExpense.type
-            .split(/,\s*|\s*&\s*|\s+and\s+/)
-            .map((word) => word.trim().toLowerCase());
+      <div className="property-section">
+        <p className="property-section-title">Utility Included</p>
+        <ul className="utilityLease-list two-column-list">
+          {recurringExpenses.map((recurringExpense, index) => {
+            // Split the type string by comma, ampersand, or 'and'
+            const formattedWords = recurringExpense.type
+              .split(/,\s*|\s*&\s*|\s+and\s+/)
+              .map((word) => word.trim().toLowerCase());
 
-          return formattedWords.map((word, wordIndex) => (
-            <li key={`${index}-${wordIndex}`}>{word}</li>
-          ));
-        })}
-      </ul>{" "}
-      <div>
-        <p className="utility-title">Lease Option</p>
-        {leaseTermList.length > 1 ? ( // Render as a list if there are multiple lease terms
-          <ul className="lease-terms-list">
+            return formattedWords.map((word, wordIndex) => (
+              <li
+                key={`${index}-${wordIndex}`}
+                className="property-section-item"
+              >
+                {word}
+              </li>
+            ));
+          })}
+        </ul>
+      </div>
+      <div className="property-section">
+        <p className="property-section-title">Lease Option</p>
+        {leaseTermList.length > 1 ? (
+          <ul className="property-section-list">
             {leaseTermList.map((term, index) => (
-              <li key={index}>{term}</li>
+              <li key={index} className="property-section-item">
+                {term}
+              </li>
             ))}
           </ul>
         ) : (
-          <p className="leaseTerms-item">{leaseTermList[0]}</p> // Render single term
+          <p className="property-section-item">{leaseTermList[0]}</p>
         )}
       </div>
-      <p className="utility-title">Property Information</p>
-      <ul>
-        <li>Built {yearBuilt}</li>
-        <li>{`${unitCount} units / ${storyCount} stories`}</li>
-        <li>{isFurnished ? "Furnished" : "Unfurnished"}</li>
-      </ul>
-    </div>
+      <div className="property-section">
+        <p className="property-section-title">Property Information</p>
+        <ul className="property-section-list two-column-list">
+          <li>Built {yearBuilt}</li>
+          <li>{`${unitCount} units / ${storyCount} stories`}</li>
+          <li>{isFurnished ? "Furnished" : "Unfurnished"}</li>
+        </ul>
+      </div>
+    </Container>
   );
 };
 
