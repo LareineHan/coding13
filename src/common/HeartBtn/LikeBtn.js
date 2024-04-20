be3162acca583b4d1fecb4a0db56446765673db3
jiylee <jjenny4711@gmail.com>
2024-04-19 23:07:25 -0400

    fixed heartBtn

--- src/common/HeartBtn/LikeBtn.js
+++ src/common/HeartBtn/LikeBtn.js
@@ -1,32 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch ,useSelector} from 'react-redux';
-import { createLike, showLikeList} from '../../redux/Actions/likeAction';
+import { createLike, showLikeList,updateSavedLikes} from '../../redux/Actions/likeAction';
 import Alert from 'react-bootstrap/Alert';
 import { Heart,HeartBK } from './Icons';
+
 const LikeBtn = React.memo(({listId}) => {
   const dispatch = useDispatch();
   const userId = sessionStorage.getItem("userId");
   const [showAlert, setShowAlert] = useState(false);
   const [like,setLike]=useState(false)
- const likeList=useSelector(state=>state.like.likeList)
-
+  
+ const savedLike=useSelector(state=>state.like.savedLike) ||{savedLike:[]}
+console.log(savedLike,'savedLike')
   useEffect(() => {
     const fetchLikeList = async () => {
       if (userId) {
         await dispatch(showLikeList(userId));
+        
       }
     };
 
     fetchLikeList();
-  }, [userId, dispatch]);
+  }, [userId]);
 
- useEffect(()=>{
-  const result=likeList?.some(item=>item.listId===listId );
-  setLike(result)
+  useEffect(() => {
   
-  console.log(result,'result')
+    const isLiked = savedLike.some(item => item.listId === listId);
+    setLike(isLiked);
+    
+  }, [savedLike, listId]);
 
- },[listId,likeList])
+
 
 
 
@@ -41,9 +45,10 @@ const LikeBtn = React.memo(({listId}) => {
   };
   
   const handleSaveClick = async () => {
-    console.log(listId, 'listId');
+    
     const newLikeStatus = !like;
     await dispatch(createLike({ userId, like: newLikeStatus, listId }));
+    dispatch(updateSavedLikes({userId,like:newLikeStatus,listId}))
        setLike(newLikeStatus);
     
  };
@@ -51,7 +56,7 @@ const LikeBtn = React.memo(({listId}) => {
   return (
     <div onClick={handleLikeClick}>
       {showAlert && <Alert>Please try to login first!</Alert>}
-      {like ? <HeartBK size={20}/> : <Heart size={20}/>}
+      {!like ? <Heart size={20}/> : <HeartBK size={20}/>}
     </div>
   );
 });
