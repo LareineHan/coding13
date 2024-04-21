a1628326ee8f71361b3277dc7cc74f567903f007
geunseonkim <guensoen@gmail.com>
2024-04-20 23:07:10 -0700

    edit responsive page

--- src/pages/Main/components/Banner/Banner.jsx
+++ src/pages/Main/components/Banner/Banner.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Banner.style.css';
 import BannerSearchBar from '../../../../common/SearchBar/BannerSearchBar';
+
 const Banner = () => {
 	return (
 		<div className='banner-container'>
 			<div className='banner-wrap'>
-				<h1>Discover Your New Home</h1>
-				<p>We've helped millions of renters find their perfect fit.</p>
+				<h1 className='banner-wrap-h1' >Discover Your New Home</h1>
+				<p className='banner-wrap-p' >We've helped millions of renters find their perfect fit.</p>
 				<div className='banner-wrap-search'>
 					<BannerSearchBar />
 				</div>
