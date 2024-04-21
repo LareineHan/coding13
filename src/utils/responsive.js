d83d6daebe995cd0722ac6b36f305386f65af7cb
jiylee <jjenny4711@gmail.com>
2024-04-21 02:53:22 -0400

    responsive.js

---
+++ src/utils/responsive.js
@@ -0,0 +1,17 @@
+export const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4,
+  
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1,
+  
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+   
+  }
+};
