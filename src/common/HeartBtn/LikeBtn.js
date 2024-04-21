ea46e6b2615172addae34a11c70139c835785e48
MJJUNG33 <jung2674@gmail.com>
2024-04-21 18:43:45 +0900

    Implement like and share buttons and style

--- src/common/HeartBtn/LikeBtn.js
+++ src/common/HeartBtn/LikeBtn.js
@@ -1,22 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch ,useSelector} from 'react-redux';
-import { createLike, showLikeList,updateSavedLikes} from '../../redux/Actions/likeAction';
-import Alert from 'react-bootstrap/Alert';
-import { Heart,HeartBK } from './Icons';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  createLike,
+  showLikeList,
+  updateSavedLikes,
+} from "../../redux/Actions/likeAction";
+import Alert from "react-bootstrap/Alert";
+import { Heart, HeartBK } from "./Icons";
 
-const LikeBtn = React.memo(({listId}) => {
+const LikeBtn = React.memo(({ listId }) => {
   const dispatch = useDispatch();
   const userId = sessionStorage.getItem("userId");
   const [showAlert, setShowAlert] = useState(false);
-  const [like,setLike]=useState(false)
-  
- const savedLike=useSelector(state=>state.like.savedLike) ||{savedLike:[]}
-console.log(savedLike,'savedLike')
+  const [like, setLike] = useState(false);
+
+  const savedLike = useSelector((state) => state.like.savedLike) || {
+    savedLike: [],
+  };
+  console.log(savedLike, "savedLike");
   useEffect(() => {
     const fetchLikeList = async () => {
       if (userId) {
         await dispatch(showLikeList(userId));
-        
       }
     };
 
@@ -24,38 +29,36 @@ console.log(savedLike,'savedLike')
   }, [userId]);
 
   useEffect(() => {
-  
-    const isLiked = savedLike.some(item => item.listId === listId);
+    const isLiked = savedLike.some((item) => item.listId === listId);
     setLike(isLiked);
-    
   }, [savedLike, listId]);
 
   const handleLikeClick = async () => {
-   
     if (!userId) {
       setShowAlert(true);
       return;
     }
-   
-  await handleSaveClick ();
+
+    await handleSaveClick();
   };
-  
+
   const handleSaveClick = async () => {
-    
     const newLikeStatus = !like;
     await dispatch(createLike({ userId, like: newLikeStatus, listId }));
-    dispatch(updateSavedLikes({userId,like:newLikeStatus,listId}))
-       setLike(newLikeStatus);
-    
- };
- 
+    dispatch(updateSavedLikes({ userId, like: newLikeStatus, listId }));
+    setLike(newLikeStatus);
+  };
+
   return (
-    <div onClick={handleLikeClick}>
+    <div className="likeBtn" onClick={handleLikeClick}>
       {showAlert && <Alert>Please try to login first!</Alert>}
-      {!like ? <Heart size={20}/> : <HeartBK size={20}/>}
+      {!like ? (
+        <Heart size={20} color="#4C4E5D" />
+      ) : (
+        <HeartBK size={20} color="#4C4E5D" />
+      )}
     </div>
   );
 });
 
 export default LikeBtn;
-
